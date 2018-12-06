6975f96ff10d4f879bcaadc516c804a2c6e0ab22
Jeremy Krovitz <jkrovitz@macalester.edu>
2018-12-05 19:37:03 -0600

    Cleaned up code. Editing and deleting categories works.
    Cleaned up code. Editing and deleting categories works. The ids aren't quite right after one category is deleted. This is a later issue to look into.

--- taskViz/routes.py
+++ taskViz/routes.py
@@ -18,18 +18,26 @@ def AuthenticationRedirect():
 @app.route("/home", methods=['GET', 'POST'])
 @login_required
 def home():
-	# new_category_form = category()
-	category_form = NewCategoryForm(request.form)
-	category_name = category_form['category_name']
-	category_color = category_form['category_color'] 
-	if request.method == 'POST':
-		new_cat = Category(category_name=category_name, category_color=category_color, is_checked=is_checked, user_id=current_user.id)
-		db.session.add(new_cat)
-		db.session.commit()
-		categories = Category.query.filter_by(user_id=current_user.id).all()
-		print(categories)
-			#category = Category.query.get(Category.category_id)
-	editCat = edit_category()
+	new_category_form = category()
+	#new_cat = Category(category_name=category_name, category_color=category_color, is_checked=False, user_id=current_user.id)
+	categories = Category.query.filter_by(user_id=current_user.id).all()
+	print(categories)
+	# tasks = Task.query.all()    # also not used ... not yet anyway...
+	return render_template('task_viz.html', categories=categories, new_category_form=new_category_form) #`new_category_form` sin't being used? should it?
+
+	'''Attempt at combining category and edit_category in 
+	the home function for popup forms. It's not working yet.  '''
+	# category_form = NewCategoryForm(request.form)
+	# category_name = category_form['category_name']
+	# category_color = category_form['category_color'] 
+	# if request.method == 'POST':
+	# 	new_cat = Category(category_name=category_name, category_color=category_color, is_checked=is_checked, user_id=current_user.id)
+	# 	db.session.add(new_cat)
+	# 	db.session.commit()
+	# 	categories = Category.query.filter_by(user_id=current_user.id).all()
+	# 	print(categories)
+	# 		#category = Category.query.get(Category.category_id)
+	# #editCat = edit_category()
 	# else:
 	# 	category = Category.query.get(Category.category_id)
 	# 	category.user_id != current_user.id
@@ -43,23 +51,11 @@ def home():
 	# 	elif request.method == 'GET':
 	# 		category_form['category_name'] = category.category_name
 	# 		category_form['category_color'] = category.category_color
-	# # tasks = Task.query.all()    # also not used
+	# tasks = Task.query.all()    # also not used
 
 		
 	
-	return render_template('task_viz.html', categories=categories, new_category_form=category_form, category_name=category_name, category_color=category_color, editCat = editCat)
-
-# wasn't being used
-# @app.route("/newcategory", methods=['GET', 'POST'])
-# def newCategoryForm():
-# 	new_category_form = NewCategoryForm()
-# 	if new_category_form.validate_on_submit():
-# 		new_category = Category(category_name=new_category_form.category_name.data, category_color=new_category_form.category_color.data)
-# 		db.session.add(new_category)
-# 		db.session.commit()
-# 		return redirect(url_for('home'))
-# 	return render_template('category_form.html', new_category_form=new_category_form)
-
+	#return render_template('task_viz.html', categories=categories, new_category_form=category_form, category_name=category_name, category_color=category_color, editCat = editCat)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():     # NOTE: when creating new account, thing to say it worked is RED. change colour later
@@ -97,79 +93,59 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-
-@app.route("/task_viz", methods=['GET', 'POST'])
+'''Eventually this function will be a route to a page where the user will be able to 
+view and edit their account info.
+'''
+@app.route("/account")
 @login_required
-def task_viz():
-	new_task_form = NewTaskForm()
-	new_category_form = NewCategoryForm()
-	print(request.method,'request method')
+def account():
+    return render_template('account.html', title='Account')
+
+@app.route('/categories', methods=['GET', 'POST'])
+def category():
+	# new_cat = Category(category_name='Math', category_color="#222", is_checked=False)
+	# db.session.add(new_cat)
+	category_name = request.form.get('category_name')
+	category_color = request.form.get('category_color')
+	category_form = NewCategoryForm(request.form)
 	if request.method == 'POST':
-		return Response(json.dumps({'id': '1', 'name': 'books', 'color': '#222'}))  # TODO: fix?
-	# if new_category_form.validate_on_submit():
-	# 	print(self.model, 'category model')
-	# 	new_category = self.model(category_id.data, category_name.data, category_color.data, is_checked.data)
-	# 	self.db.session.add(new_category)
-	# 	self.db.session.commit()
-	# 	return redirect(url_for(task_viz))
+		print(category_name, category_color)
+		new_cat = Category(category_name=category_name, category_color=category_color, is_checked=False, user_id=current_user.id)
+		db.session.add(new_cat)
+		db.session.commit()
+		print(Category.query.all())
+		# return Response({})
+		return redirect(url_for('home'))
+	cat = Category.query.all()
+	print(cat, 'categories')
+	# return Response(json.dumps([]))
 
-	if request.method == 'POST':
-		print(new_task_form.data, 'NewTaskForm')
-
-	return render_template(
-		'task_viz.html',
-		new_category_form=new_category_form,
-		new_task_form=new_task_form
-		)
-
+	return render_template('forms/category_form.html', new_category_form=category_form, category_name=category_name, category_color=category_color, edit_bool=False)
 
 @app.route("/category/<int:category_id>")
 @login_required
 def get_category_id(category_id):
 	category = Category.query.get_or_404(category_id)
-	#return render_template('category.html', category_id=category.category_id, category_name=category.category_name, category=category, user_id=current_user.id)
+	return render_template('category.html', category_id=category.category_id, category_name=category.category_name, category=category, user_id=current_user.id)
 
-# @app.route("/category/<int:category_id>/edit", methods=['GET', 'POST'])
-# @login_required
-# def edit_category(category_id):
-#     category = Category.query.get(category_id)
-#     form = NewCategoryForm()
-#     if category.user_id != current_user.id:
-#         abort(403)
-#     if request.method == 'POST':
-#         category.category_name=request.form['category_name']
-#         category.category_color=request.form['category_color']
-#         db.session.commit()
-#         flash('Your category has been updated!', 'success')
-#         #return redirect(url_for('home', category=category_id))
-#     elif request.method == 'GET':
-#         form.category_name.data = category.category_name
-#         form.category_color.data = category.category_color
-#     return render_template('forms/category_form.html',
-#                            new_category_form=form)
-
-
-
-# @app.route("/category/<int:category_id>/edit", methods=['GET', 'POST'])
-# @login_required
-def edit_category():
-    category = Category.query.get(Category.category_id)
-    category_form = NewCategoryForm()
+@app.route("/category/<int:category_id>/edit", methods=['GET', 'POST'])
+@login_required
+def edit_category(category_id):
+    category = Category.query.get(category_id)
+    form = NewCategoryForm()
     if category.user_id != current_user.id:
         abort(403)
     if request.method == 'POST':
-        category.category_name=request.category_form['category_name']
-        category.category_color=request.category_form['category_color']
+        category.category_name=request.form['category_name']
+        category.category_color=request.form['category_color']
         db.session.commit()
         flash('Your category has been updated!', 'success')
-        #return redirect(url_for('home', category=category_id))
+        return redirect(url_for('home', category=category_id))
     elif request.method == 'GET':
-        category_form['category_name'] = category.category_name
-        category_form['category_color'] = category.category_color
-    # return render_template('forms/category_form.html',
-    #                        new_category_form=category_form)
-
-
+        form.category_name.data = category.category_name
+        form.category_color.data = category.category_color
+    return render_template('forms/category_form.html',
+                           new_category_form=form)
 
 #We should decide where we want to have the options for deleting categories.
 @app.route("/category/<int:category_id>/delete", methods=['POST'])
@@ -182,7 +158,6 @@ def delete_category(category_id):
     db.session.commit()
     flash('Your category has been deleted!', 'success')
     return redirect(url_for('home'))
-
 
 
 # @app.route('/categories', methods=['GET', 'POST'])
@@ -214,8 +189,26 @@ def delete_category(category_id):
 	# return render_template('forms/category_form.html', new_category_form=category_form, edit_bool=False, category_name=category_name, category_color=category_color)
 
 
-
-@app.route("/account")
+@app.route("/task_viz", methods=['GET', 'POST'])
 @login_required
-def account():
-    return render_template('account.html', title='Account')
+def task_viz():
+	new_task_form = NewTaskForm()
+	new_category_form = NewCategoryForm()
+	print(request.method,'request method')
+	if request.method == 'POST':
+		return Response(json.dumps({'id': '1', 'name': 'books', 'color': '#222'}))  # TODO: fix?
+	# if new_category_form.validate_on_submit():
+	# 	print(self.model, 'category model')
+	# 	new_category = self.model(category_id.data, category_name.data, category_color.data, is_checked.data)
+	# 	self.db.session.add(new_category)
+	# 	self.db.session.commit()
+	# 	return redirect(url_for(task_viz))
+
+	if request.method == 'POST':
+		print(new_task_form.data, 'NewTaskForm')
+
+	return render_template(
+		'task_viz.html',
+		new_category_form=new_category_form,
+		new_task_form=new_task_form
+		)
